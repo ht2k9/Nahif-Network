9f90e14297afb89a903a131cda4d5b54701258e1
ht2k9 <abbasa4696@gmail.com>
2019-07-01 19:14:06 +0300

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const PORT = process.env.PORT || 8080;
 
 const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.set('view engine' , 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -13,6 +16,15 @@ app.get('/', (req, res) => {
 
 app.get('/:channel', (req, res) => {
     res.render(req.params.channel);
+});
+
+app.post('/register', (req, res) => {
+    let data = '\n name: ' + req.body.name + ' | phone: ' + req.body.phone;
+    fs.appendFile('data.txt', data , function (err) {
+        if (err) throw err;
+        
+        res.redirect('/');
+      });
 });
 
 
